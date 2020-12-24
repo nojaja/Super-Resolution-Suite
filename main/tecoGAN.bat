e5622751ad1b7032aeeb9c35ef57a3c7739c1944
nojaja <free.riccia@gmail.com>
2020-12-24 18:53:45 +0900

    GPUを利用するように修正 waifu2xのスケールの設定変更 ffmpegでもGPU利用するようにh264_nvencに変更

--- main/tecoGAN.bat
+++ main/tecoGAN.bat
@@ -9,9 +9,10 @@ cd tecoGAN
 set filename=%1
 echo filename : %filename%
 
-@echo on
 :分割箇所抽出
+echo ==================================
 echo 分割箇所抽出
+echo ==================================
 
 IF NOT EXIST %current_dir%LR\catalog (
 mkdir %current_dir%LR\catalog
@@ -42,10 +43,14 @@ for /f "usebackq" %%n in (`powershell -c "&{%%a/%time_base%}"`) do @set en=%%n
 set splitid=0!num!
 set splitid=!splitid:~-2,2!
 
+echo ==================================
 echo 処理開始 split!splitid! !st!-!en!
+echo ==================================
 
 :分割
+echo ----------------------------------
 echo 分割 split!splitid! !st!-!en!
+echo ----------------------------------
 IF NOT EXIST %current_dir%LR/split!splitid!/ (
 mkdir %current_dir%LR\split!splitid!
 ffmpeg -y -i %filename% -vcodec png -ss !st! -to !en! %current_dir%LR/split!splitid!/image_%%05d.png
@@ -54,21 +59,27 @@ echo 分割 split!splitid! !st!-!en! skip
 )
 
 :waifu2x
+echo ----------------------------------
 echo waifu2x split!splitid! !st!-!en!
+echo ----------------------------------
 mkdir %current_dir%LR\split!splitid!_2x
 rem waifu2x-caffe-cui --model_dir %current_dir%waifu2x-caffe/models/upconv_7_photo -i LR/split!splitid!/ -m noise --noise_level 3 -p cpu --tta 1 --model_type upconv_7_photo -o LR/split!splitid!_2x/
-waifu2x-ncnn-vulkan -m models-upconv_7_photo -i %current_dir%LR/split!splitid!/ -o %current_dir%LR/split!splitid!_2x/ -n 3 -s 2 -x
+waifu2x-ncnn-vulkan -m models-upconv_7_photo -i %current_dir%LR/split!splitid!/ -o %current_dir%LR/split!splitid!_2x/ -n 3 -s 1 -j 4,4,4 -x
 
 :TecoGAN
+echo ----------------------------------
 echo TecoGAN split!splitid! !st!-!en!
+echo ----------------------------------
 python main.py --cudaID 0 --output_dir %current_dir%results/ --summary_dir %current_dir%results/log/ ^
 --mode inference --input_dir_LR %current_dir%LR/split!splitid!_2x --output_pre split!splitid!_2x  ^
 --num_resblock 16  --checkpoint ./model/TecoGAN --output_ext png
 
 :エンコード
+echo ----------------------------------
 echo ffmpeg split!splitid! !st!-!en!
+echo ----------------------------------
 IF NOT EXIST %current_dir%results/split!splitid!_2x.mp4 (
-ffmpeg -y -r %fps% -i %current_dir%results/split!splitid!_2x/output_image_%%05d.png -vcodec libx264 -pix_fmt yuv420p %current_dir%results/split!splitid!_2x.mp4
+ffmpeg -y -r %fps% -i %current_dir%results/split!splitid!_2x/output_image_%%05d.png -vcodec h264_nvenc -pix_fmt yuv420p %current_dir%results/split!splitid!_2x.mp4
 ) ELSE (
 echo ffmpeg split!splitid! !st!-!en! skip
 )
