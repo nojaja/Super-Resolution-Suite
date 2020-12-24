d7fecfe87247e5739b9ba73fd92eb6c039a50b09
nojaja <free.riccia@gmail.com>
2020-12-24 18:51:48 +0900

    CUDA_PATHから必要なDLL取得する処理修正

--- copy_cuda.bat
+++ copy_cuda.bat
@@ -1,33 +1,39 @@
-call envset.bat
+@setlocal enabledelayedexpansion
 
-:set cudapath="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0\bin"
-set cudapath=.\cuda10
-copy %cudapath%\cublas64_100.dll %python_venv% /v /y
-copy %cudapath%\cudart32_100.dll %python_venv% /v /y
-copy %cudapath%\cudart64_100.dll %python_venv% /v /y
-copy %cudapath%\cudnn64_7.dll %python_venv% /v /y
-copy %cudapath%\cufft64_100.dll %python_venv% /v /y
-copy %cudapath%\cufftw64_100.dll %python_venv% /v /y
-copy %cudapath%\cuinj64_100.dll %python_venv% /v /y
-copy %cudapath%\curand64_100.dll %python_venv% /v /y
-copy %cudapath%\cusolver64_100.dll %python_venv% /v /y
-copy %cudapath%\cusparse64_100.dll %python_venv% /v /y
+set current_dir=%~dp0
+PATH=%~dp0\dist\7-zip;%PATH%
+set python_venv=%~dp0\dist\env
+
+IF NOT EXIST "%CUDA_PATH%" (exit)
+
+:set CUDA_PATH="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0\bin"
+set CUDA_PATH=.\cuda10
+copy %CUDA_PATH%\bin\cublas64_*.dll %python_venv% /v /y
+copy %CUDA_PATH%\bin\cudart32_*.dll %python_venv% /v /y
+copy %CUDA_PATH%\bin\cudart64_*.dll %python_venv% /v /y
+copy %CUDA_PATH%\bin\cudnn64_*.dll %python_venv% /v /y
+copy %CUDA_PATH%\bin\cufft64_*.dll %python_venv% /v /y
+copy %CUDA_PATH%\bin\cufftw64_*.dll %python_venv% /v /y
+copy %CUDA_PATH%\bin\cuinj64_*.dll %python_venv% /v /y
+copy %CUDA_PATH%\bin\curand64_*.dll %python_venv% /v /y
+copy %CUDA_PATH%\bin\cusolver64_*.dll %python_venv% /v /y
+copy %CUDA_PATH%\bin\cusparse64_*.dll %python_venv% /v /y
 
 pause
 exit
 
-copy %cudapath%\nppc64_100.dll %python_venv% /v /y
-copy %cudapath%\nppial64_100.dll %python_venv% /v /y
-copy %cudapath%\nppicc64_100.dll %python_venv% /v /y
-copy %cudapath%\nppicom64_100.dll %python_venv% /v /y
-copy %cudapath%\nppidei64_100.dll %python_venv% /v /y
-copy %cudapath%\nppif64_100.dll %python_venv% /v /y
-copy %cudapath%\nppig64_100.dll %python_venv% /v /y
-copy %cudapath%\nppim64_100.dll %python_venv% /v /y
-copy %cudapath%\nppist64_100.dll %python_venv% /v /y
-copy %cudapath%\nppisu64_100.dll %python_venv% /v /y
-copy %cudapath%\nppitc64_100.dll %python_venv% /v /y
-copy %cudapath%\npps64_100.dll %python_venv% /v /y
-copy %cudapath%\nvblas64_100.dll %python_venv% /v /y
-copy %cudapath%\nvgraph64_100.dll %python_venv% /v /y
-copy %cudapath%\nvrtc-builtins64_100.dll %python_venv% /v /y
+copy %CUDA_PATH%\nppc64_100.dll %python_venv% /v /y
+copy %CUDA_PATH%\nppial64_100.dll %python_venv% /v /y
+copy %CUDA_PATH%\nppicc64_100.dll %python_venv% /v /y
+copy %CUDA_PATH%\nppicom64_100.dll %python_venv% /v /y
+copy %CUDA_PATH%\nppidei64_100.dll %python_venv% /v /y
+copy %CUDA_PATH%\nppif64_100.dll %python_venv% /v /y
+copy %CUDA_PATH%\nppig64_100.dll %python_venv% /v /y
+copy %CUDA_PATH%\nppim64_100.dll %python_venv% /v /y
+copy %CUDA_PATH%\nppist64_100.dll %python_venv% /v /y
+copy %CUDA_PATH%\nppisu64_100.dll %python_venv% /v /y
+copy %CUDA_PATH%\nppitc64_100.dll %python_venv% /v /y
+copy %CUDA_PATH%\npps64_100.dll %python_venv% /v /y
+copy %CUDA_PATH%\nvblas64_100.dll %python_venv% /v /y
+copy %CUDA_PATH%\nvgraph64_100.dll %python_venv% /v /y
+copy %CUDA_PATH%\nvrtc-builtins64_100.dll %python_venv% /v /y
