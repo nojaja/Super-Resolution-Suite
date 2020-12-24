b43c10d52625e17af4a17343ede56e9172b25962
nojaja <free.riccia@gmail.com>
2020-12-24 18:52:18 +0900

    initで必要なモジュールを全て取得するようにした

--- init.bat
+++ init.bat
@@ -2,6 +2,7 @@
 
 set current_dir=%~dp0
 PATH=%~dp0\dist\7-zip;%PATH%
+set python_venv=%~dp0\dist\env
 
 rem xcopy main dist\main /e/h/c/i/q
 xcopy main dist /e/h/c/i/q
@@ -16,12 +17,47 @@ mkdir dist\TecoGAN\model
 
 curl -o ./dist/tmp/python.zip https://www.python.org/ftp/python/3.7.9/python-3.7.9-embed-amd64.zip
 tar -xf ./dist/tmp/python.zip -C ./dist/env/
+PATH=%python_venv%;%python_venv%\Library\mingw-w64\bin;%python_venv%\Library\usr\bin;%python_venv%\Library\bin;%python_venv%\Scripts;%python_venv%\bin;%PATH%
+
+rem Microsoft Visual C++ 2015 再頒布可能パッケージ Update 3
+rem https://www.microsoft.com/ja-jp/download/confirmation.aspx?id=53587&6B49FDFB-8E5B-4B07-BC31-15695C5A2143=1
 
 curl -o ./dist/env/get-pip.py https://bootstrap.pypa.io/get-pip.py
-rem python ./dist/get-pip.py
+python ./dist/env/get-pip.py
+
+rem echo import site >> ./dist/env/python37._pth
+rem echo ./Lib/site-packages >> ./dist/env/python37._pth
+del /Y ./dist/env/python37._pth
+
+rem curl -o ./dist/env/termcolor.whl https://download.lfd.uci.edu/pythonlibs/z4tqcw5k/termcolor-1.1.0-py2.py3-none-any.whl
+
+rem pip install ./dist/env/termcolor.whl
+
+rem pip install youtube-dl
+rem pip install future
+pip install certifi
+pip install wincertstore
+
+rem pip install importlib-metadata==1.7.0
+rem pip install traitlets==4.3.3
+
+rem pip3 install --ignore-installed --upgrade tensorflow-gpu # or tensorflow
+pip install tensorflow-gpu==1.15.4
+rem pip install tensorflow==1.15.4
+pip install keras==2.3.1
+rem pip install keras-gpu
+rem pip install opencv
+pip install opencv-python==4.3.0.36
+pip install scikit-image==0.17.2
+pip install numpy==1.16.0
+
+pip3 install -r ./dist/TecoGAN/requirements.txt
 
 curl -o ./dist/tmp/model.zip https://ge.in.tum.de/download/data/TecoGAN/model.zip
 tar -xf ./dist/tmp/model.zip -C ./dist/TecoGAN/model
+
+curl -o ./dist/tmp/ofrvsr.zip http://ge.in.tum.de/download/2019-TecoGAN/FRVSR_Ours.zip
+tar -xf ./dist/tmp/ofrvsr.zip -C ./dist/TecoGAN/model
 
 curl -L -o ./dist/tmp/waifu2x-ncnn-vulkan.zip https://github.com/nihui/waifu2x-ncnn-vulkan/releases/download/20200818/waifu2x-ncnn-vulkan-20200818-windows.zip
 rem tar -xf ./dist/tmp/waifu2x-ncnn-vulkan.zip -C ./dist/waifu2x
@@ -38,3 +74,4 @@ for /d %i in (dist\ffmpeg_\*) do (rename .\%i ffmpeg)
 move /Y .\dist\ffmpeg_\ffmpeg .\dist\ffmpeg
 rd .\dist\ffmpeg_
 
+rem CUDA_PATH
